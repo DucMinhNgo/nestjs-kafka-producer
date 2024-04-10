9a899e8b8083f11eecff9c7d1d3380afa01bfd23
ducngo <duc.ngo@salto.vn>
2024-04-10 10:09:10 +0700

    feat: add send to kafka

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from "@nestjs/microservices";
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'kafkaSample',
+        brokers: [process.env.KAFKA_BROKER],
+      },
+      consumer: {
+        groupId: 'test' // Should be the same thing we give in consumer
+      }
+    }
+  })
+  client: ClientKafka;
+
+  async onModuleInit() {
+    // Need to subscribe to topic 
+    // so that we can get the response from kafka microservice
+    this.client.subscribeToResponseOf('notifications');
+    await this.client.connect();
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return this.client.send('notifications', 'producer send kafka'); // args - topic, message
   }
 }
